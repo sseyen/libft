6388d8bab0c6963593c3525bbe291af348770863
Alex <krisstalexx@gmail.com>
2024-09-21 12:08:34 +0200

    Part1 + Part2 half

---
+++ srcs/ft_strlen.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 13:27:11 by alisseye          #+#    #+#             */
+/*   Updated: 2024/09/19 20:34:30 by alisseye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while(str[len])
+		len++;
+	return (len);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	printf("%d\n", ft_strlen("####"));
+// }
